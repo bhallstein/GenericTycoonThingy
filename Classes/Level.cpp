0a88c58510713dc98dbd302de0cb5187fe7be533
Ben <ben@Fratercula.local>
2017-08-16 22:40:16 +0100

    Changed LevelMap to Level, overwriting previous

--- Classes/Level.cpp
+++ Classes/Level.cpp
@@ -1,55 +1,85 @@
-#include <sstream>
-#include "fstream"
-#include "Level.h"
-Level::Level(Builder& bill)//maybe add string, to load up fixed level - string could be provided by a file which man reads
+#include "LevelMap.h"
+
+/*********************************
+ *   MapLocation implementation  *
+ *********************************/
+
+MapLocation::MapLocation() {
+	// Constructor
+}
+MapLocation::~MapLocation() {
+	// Destructor
+}
+
+void MapLocation::addBuilding(Building *b) {
+	// Add a building to the list thereof
+	buildings.push_back(b);
+}
+void MapLocation::removeBuilding(Building *b) {
+	// Remove a bulding from the list thereof
+	buildings.remove(b);
+}
+
+
+
+/*********************************
+ *    LevelMap implementation    *
+ *********************************/
+
+LevelMap::LevelMap(int _columns, int _rows, int _width, int _height)
 {
-	//read data from a file, create appropriate level
-	std::string output;
-	std::ifstream myfile ("Levels_Data.txt");
-	if (myfile.is_open())
-  {
-    while (! myfile.eof() )//eof = end of file
-    {
-		getline (myfile,output);
-		std::string buf; // Have a buffer string
-		std::stringstream ss(output); // Insert the string into a stream
-		std::vector<std::string> tokens;
-		while (ss >> buf)
-		{
-			tokens.push_back(buf);//can be redone - see below. Take first word, then keep the rest (allow for multi word add)
-		}
-		std::string x = tokens[1];
-		bill.addAddress(x);
-    }
-    myfile.close();
-  }
-	else
-	{
-		std::cout << "Unable to open file";
-	}
+	rows = _rows, columns = _columns;
+	block_size.x = _width/_columns;
+	block_size.y = _height/_rows;
 	
-	//load up all building data
-	std::ifstream BuildingInfo ("Building_Data2.txt");
-	if(BuildingInfo.is_open())
-	{
-		std::vector<std::string> tokens;
-		while(! BuildingInfo.eof())
-		{
-			getline(BuildingInfo, output);
-			tokens.push_back(output);			
+	maplocs.resize(block_size.x * block_size.y);
+}
+LevelMap::~LevelMap()
+{
+	// Destructor
+	maplocs.clear();
+}
+
+Building* LevelMap::createBuilding()
+{
+	Building b(block_size);
+	buildings.push_back(b);
+	std::cout << "created building: " << buildings.size() << std::endl;
+
+	Building *new_building = &buildings.back();
+
+	return new_building;
+}
+
+//void LevelMap::memmap_addObject(DrawnObject *object) {
+//	// Add to memory map
+//}
+//void LevelMap::memmap_removeObject(DrawnObject *object) {
+//	// Remove from memory map
+//	// Should a drawn object always implement a prev_position, with the option of NO_PREV_POSITION?
+//	// This way, can remove it from memory map without passing x1, y1, x2, y2
+//	// Yes, then.
+//}
+
+void LevelMap::draw(sf::RenderWindow &window)
+{
+	// Draw buildings
+	for (vector<Building>::iterator i = buildings.begin(); i < buildings.end(); i++) {
+		if ((*i).destroyed) {
+			// Remove from memory map
+			// this->memmap_removeObject(buildings[i]);
+			buildings.erase(i--);
 		}
-		bill.LoadStructures(tokens);
+		//else if ((*i).position_updated) {
+		//	// Move in memory map
+		//	this->memmap_removeObject(buildings[i]);
+		//	this->memmap_addObject(buildings[i]);
+		//	(*i).position_updated = false;
+		//	//int x1, y1, x2, y2;
+		//	//new_building->getBounds(&x1, &y1, &x2, &y2);
+		//	//this->memmap_addObject(new_building, x1, y1, x2, y2);
+		//}
+		else
+			(*i).draw(window);
 	}
-
 }
-
-Level::~Level(void)
-{
-	//bill.clear function - delete it all
-}
-
-void Level::eventCheck(int day)
-{
-	//as days pass, fixed things happen in a level - should also be read from a file...
-}
-
--- Classes/Level.h
+++ Classes/Level.h
@@ -1,18 +1,58 @@
-#ifndef LEVEL_H
-#define LEVEL_H
+/*
+ * LevelMap.h - Defines the map data, including buildings and units, for a level
+ *
+ */
+#ifndef LEVELMAP_H
+#define LEVELMAP_H
 
-#include "Builder.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include "Building.h"
 
-class Level
+using namespace std;
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+class MapLocation {
+public:
+	MapLocation();
+	~MapLocation();
+
+	void addBuilding(Building* b);
+	void removeBuilding(Building* b);
+private:
+	// "Compared to other vectors and deques, lists perform better in inserting, extracting and moving elements..."
+	//List<Unit*> units;
+	list<Building*> buildings;
+};
+
+
+
+class LevelMap
 {
 public:
-	Level(Builder& bill);
-	~Level(void);
-	void eventCheck(int day);
+	LevelMap(int _rows, int _columns, int _width, int _height);
+	~LevelMap();
 
-private:
+	// Functions
+	void draw(sf::RenderWindow& window);
+	Building *createBuilding();
 
+	//void memmap_addObject(DrawnObject *object);
+	//void memmap_removeObject(DrawnObject *object);
 
+	// Properties
+	int columns, rows;
+	vector<Building> buildings;
+
+protected:
+	// Memory mapped list of things at each integer location
+	vector<MapLocation*> maplocs;
+
+	// Pixel size of a block on the map
+	sf::Vector2i block_size;
 };
 
 #endif
