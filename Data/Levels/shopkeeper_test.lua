799fab2d68c86290db685dcc29c37236a57b56e5
Ben <ben@Fratercula.local>
2017-09-16 19:16:27 +0100

    Add shopkeeper_test level

---
+++ Data/Levels/shopkeeper_test.lua
@@ -0,0 +1,65 @@
+-- a save game
+
+levelData = {
+	monetaryTarget = 1200,
+	timeLimit = 300
+}
+
+playerState = {
+	balance = 10
+}
+
+mapData = {
+	width = 55,
+	height = 45,
+	
+	buildings = {
+		{
+			rct = {
+				position = { x=13,y=9,a=0,b=0 },
+			},
+			uid = 348756,
+			type = "shop",
+			destroyed = false,
+		}
+	},
+	
+	units = {
+		{
+			destroyed = false,
+			rct = {
+				position = { x=4,y=10,a=0,b=0 },
+				size = { width=1,height=1 }
+			},
+			type = "shopkeeper",
+			uid = 1,
+			controller = 2,
+			dest = { x=4,y=10,a=0,b=0 },
+			hired = false,
+			mode = "IDLE",
+			route = {  }
+		},
+
+	},
+	furnishings = {
+		{
+			destroyed = false,
+			rct = {
+				position = { x=0,y=0,a=0,b=0 },
+				size = { width=2,height=2 }
+			},
+			type = "barberschair",
+			uid = 3,
+			purchased = true
+		}
+	},
+	spawnPoints = {
+		
+	},
+	controllers = {
+		
+	},
+	inactiveControllers = {
+		
+	}
+}
